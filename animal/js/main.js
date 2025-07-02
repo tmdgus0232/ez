ac149568fbb5904b79e911ce3700054dd1bd738b
tmdgus0232 <tmdgus0232@gmail.com>
2025-07-02 17:49:32 +0900

    250702

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,175 +1,159 @@
+
 $(document).ready(function(){
-    /************************************************** header **************************************************
-        * pc인지 모바일인지 구분 - 브라우저 넓이로
-        * 스크롤 값 계산
-        * 공통사항 : 브라우저가 스크롤되면 or header에 오버하면 header에 fixed 클래스 추가
-        * pc일때 마우스를 오버한 li에만 over 클래스 추가
-        * 모바일일 때 메뉴열기를 클릭하면 header에 menu_open 클래스 추가
-        * 하위 메뉴가 존재하는 1차 메뉴를 클릭하면 클릭한 li에 open 클래스 추가
-    */
-   
-    let device_status; // 모바일인지 pc인지
-    let scrolling; // 스크롤한 값
-    let window_w; // 브라우저 넓이
-    let mobile_size = 1024; // 모바일로 전환되는 사이즈
     
-    resize_chk(); // 함수 실행(문서가 처음 로딩되었을 때 한번)
-    scroll_chk(); // 함수 실행(문서가 처음 로딩되었을 때 한번)
+    /*************************** header 와 메뉴 : 시작 *************************
+    * pc인지 모바일인지 구분 - 브라우저 넓이로.. 
+    * 스크롤값 계산 
+    * 공통사항 : 브라우저가 스크롤되면 OR header에 오버하면 header에 fixed 클래스를 추가
+    * PC일때 : 마우스를 오버한 li에만 over 클래스 추가
+    * 모바일때 : 메뉴열기를 클릭하면 header에 menu_open 클래스 추가
+    *           1차메뉴를 클릭하면 (하위메뉴가 있는 1차메뉴만)  클릭한 li에 open 클래스 추가
+    *****/
 
-    // 브라우저가 리사이즈 될 때마다 한번씩 실행
-    $(window).resize(function(){
-        resize_chk();
-    });
+    let device_status //모바일인지 pc인지
+    let scrolling //스크롤한 값
+    let window_w //브라우저 넓이
+    let mobile_size = 1024 //모바일로 전환되는 사이즈
 
-    $(window).scroll(function(){
-        scroll_chk();
-    });
+    scroll_chk() //함수실행 (처음에 문서가 로딩되었을때 1번)
+    resize_chk() //함수실행
+    $(window).resize(function(){ //브라우저가 리사이즈될때마다 1번씩 실행
+        resize_chk() //함수실행
+    })
+    $(window).scroll(function(){ //브라우저를 스크롤 할때마다 1번씩 실행
+        scroll_chk() //함수실행
+    })
 
-    // 함수 선언
+    function scroll_chk(){ //함수선언
+        scrolling = $(window).scrollTop()
+        //console.log(scrolling)
+        if(scrolling > 0){
+            $('header').addClass('fixed')
+            //console.log('scroll 너냐??')
+        }else{
+            $('header').removeClass('fixed')
+        }
+    }
     function resize_chk(){
-        window_w = $(window).width();
-        if(window_w > mobile_size)
-        {
-            device_status = 'pc';
+        window_w = $(window).width()
+        if(window_w > mobile_size){
+            device_status = 'pc'
+        }else{
+            device_status = 'mobile'
         }
-        else
-        {
-            device_status = 'mobile';
-        }
-    };
+        //console.log(device_status)
+    }
 
-    function scroll_chk(){
-        scrolling = $(window).scrollTop();
-        if(scrolling > 0)
-        {
-            $('header').addClass('fixed');
-        }
-        else
-        {
-            $('header').removeClass('fixed');
-        }
-    };
-
-    // header에 마우스를 오버했을 때, 클릭했을 때도 작동함
+    /* header에 마우스를 오버했을때 -- 클릭했을때도 작동함 */
     $('header').on('mouseenter focusin', function(){
-        if(device_status == 'pc')
-        {
-            $('header').addClass('fixed');
-        }
-    });
-    
+        if(device_status == 'pc'){
+            $('header').addClass('fixed')
+            //console.log('mouseenter 너냐??')
+        }        
+    })
     $('header').on('mouseleave focusout', function(){
-        // 브라우저가 스크롤 된 상태에서는 header에 fixed 클래스를 삭제하면 안됨, 맨 위에 있을 때만 삭제해야 함
-        if(scrolling == 0)
-        {
-            $('header').removeClass('fixed');
-        }
-    });
+        /* 브라우저가 스크롤된 상태에서는 header에 fixed 클래스를 삭제하면 안됨
+           맨 위에 있을때만 삭제해야함  */
+        if(scrolling <= 0){
+            //console.log('아웃')
+            $('header').removeClass('fixed')
+        }//if종료
+    })
 
     $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin', function(){
-        if(device_status == 'pc')
-        {
-            $(this).addClass('over');
+        if(device_status == 'pc'){
+            //console.log('오버했다!!!!!!!!!!!!!!!!')
+            $(this).addClass('over')
         }
-    });
-
-    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
-        $(this).removeClass('over');
-    });
+    })
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave ', function(){
+        //console.log('아웃!!!!!!')
+        $(this).removeClass('over')
+    })
+    $('header .gnb .gnb_wrap ul.depth1 > li > ul.depth2 > li:last-child').on('focusout', function(){
+        $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+    })
 
     $('header .gnb .gnb_open').on('click', function(){
-        $('header').addClass('menu_open');
-    });
-
-    $('header .gnb .gnb_wrap ul.depth1 > li > ul.depth2 > li:last-child').on('focusout', function(){
-        $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over');
-    });
-
+        $('header').addClass('menu_open')
+    })
     $('header .gnb .gnb_close').on('click', function(){
-        $('header').removeClass('menu_open');
-    });
-
+        $('header').removeClass('menu_open')
+    })
     /*
-        닫힌 메뉴를 클릭하면 열리고, 열린 메뉴를 클릭하면 닫힘
-        동시에 여러 개의 메뉴가 열려있을 수도 있음
+        닫힌메뉴를 클릭하면 열리고, 열린메뉴를 클릭하면 닫힘
+        동시에 여러개의 메뉴가 열려있을 수도 있음
         toggleClass - 클래스가 없으면 추가하고, 있으면 삭제
     */
-    $('header .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
-        if(device_status == 'mobile')
-        {
-            e.preventDefault();
-            $(this).parents('li').toggleClass('open');
+    $('header .gnb .gnb_wrap ul.depth1 > li:has(ul.depth2) > a').on('click', function(e){
+        if(device_status == 'mobile'){
+            e.preventDefault()
+            console.log('클릭했다!!!!!!!!!!!')
+            $(this).parents('li').toggleClass('open')
         }
-    });
-    
-    /************************************************** visual swiper **************************************************/
-    
+    })
+
+    /*************************** header 와 메뉴 : 종료 *****************************/
+
+    /*************************** visual swiper : 시작 *****************************/
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 5000,
-            disableOnInteraction: true,
+             delay: 5000,
+             disableOnInteraction: true,
         },
-
-        effect: "fade", /* fade 효과 */
-
+        //effect: "fade", /* fade 효과 */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.swiper-pagination', /* 해당 요소의 class명 */
-            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-            renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-                return '<span class="' + className + '">' + (index + 1) + "</span>";
-            },
-        },
-        
-
         navigation: {  /* 이전, 다음 버튼 */
             nextEl: '.visual .btn_wrap button.btn_next',  /* 다음 버튼의 클래스명 */
             prevEl: '.visual .btn_wrap button.btn_prev',  
         },
-
     });
-    // visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-    // visual_swiper.autoplay.start();  /* 재생 기능 */
-    $('.visual .btn_wrap button.btn_stop').on('click', function(){ // 일시정지
-        visual_swiper.autoplay.stop();
-        $(this).hide(); // 정지 숨김
-        $('.visual .btn_wrap button.btn_play').show(); // 재생 보임
-    });
-    $('.visual .btn_wrap button.btn_play').on('click', function(){ // 재생
-        visual_swiper.autoplay.start();
-        $(this).hide(); // 재생 숨김
-        $('.visual .btn_wrap button.btn_stop').show(); // 정지 보임
-    });
+    $('.visual .btn_wrap button.btn_stop').on('click', function(){
+        //console.log('일시정지 버튼 클릭')
+        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide() //정지버튼 자신은 숨김
+        $('.visual .btn_wrap button.btn_play').show() //재생 나타남
+    })
+    $('.visual .btn_wrap button.btn_play').on('click', function(){
+        //console.log('재생 버튼 클릭')
+        visual_swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide() //재생버튼 자신은 숨김
+        $('.visual .btn_wrap button.btn_stop').show() //정지버튼이 나타남
+    })
+    /*************************** visual swiper : 끝 *****************************/
 
-    /************************************************** find **************************************************/
-    
-    let find_content; // 클릭한 메뉴 이름(id)
-
+    /**************************** find 탭 기능 : 시작 ***************
+     * 1. 클릭한 li에서 data-content 값을 가져와서
+     *    ==> tab_item 중에 해당 값이 id인 요소를 찾아서 나타나게 해야함 (다른 요소는 숨김)
+     * 2. 클릭한 li에만 active 클래스 줌 
+     * 3. 클릭한 li안에 있는 span에 선택됨이라고 글자 써줌 (다른 li에 있는 건 삭제)
+     * 4. 클릭한 li 속성 aria-selected값을 true로 변경 (다른 li는 모두 false)
+     * ****** */
+    let find_content //클릭한 메뉴의 이름(id)
     $('.find .list .tab_list ul li').on('click', function(){
         
-        if($(this).hasClass('active') == false)
-        {
-            find_content = $(this).attr('data-content');
+        if($(this).hasClass('active') == false){
+            //console.log('선택안된메뉴')
+            find_content = $(this).attr('data-content')
+            //console.log(find_content)
+            $('.find .list .tab_content .tab_item').removeClass('active')
+            $('.find .list .tab_content').find('#'+find_content).addClass('active')
 
-            $('.find .list .tab_content .tab_item').removeClass('active');
-            $('.find .list .tab_content').find('#'+find_content).addClass('active');
+            $('.find .list .tab_list ul li').removeClass('active')
+            $(this).addClass('active')
 
-            $('.find .list .tab_list ul li').removeClass('active');
-            $(this).addClass('active');
+            $('.find .list .tab_list ul li button span').text('')
+            $(this).find('span').text('선택됨')
 
-            $('.find .list .tab_list ul li button span').text('');
-            $(this).find('span').text('선택됨');
-
-            $('.find .list .tab_list ul li').attr('aria-selected', 'false');
-            $(this).attr('aria-selected', 'true');
+            $('.find .list .tab_list ul li').attr('aria-selected', 'false')
+            $(this).attr('aria-selected', 'true')
         }
-    });
+    })
 
-    /************************************************** adopt swiper **************************************************/
+    /**************************** find 탭 기능 : 끝 ********************* */
 
+
+    /****************************** 분양 swiper : 시작 ****************************/
     const adopt_swiper = new Swiper('.adopt .swiper', { /* 팝업을 감싼는 요소의 class명 */
         slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
@@ -181,10 +165,47 @@ $(document).ready(function(){
         },
         centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
         navigation: {
             nextEl: '.adopt .list_ctrl .btn_next',
             prevEl: '.adopt .list_ctrl .btn_prev',
         },
     });
-});
+
+    /****************************** 분양 swiper : 종료 ****************************/
+
+    /******************************** 후기 swiper : 시작 ************************** */
+    const review_swiper = new Swiper('.review .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+            1024: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+        navigation: {
+            nextEl: '.review .list .btn_next',
+            prevEl: '.review .list .btn_prev',
+        },
+    });
+    // review_swiper.autoplay.stop();  /* 일시정지 기능 */
+    // review_swiper.autoplay.start();  /* 재생 기능 */
+
+    /******************************** 후기 swiper : 끝 ************************** */
+
+    $('footer .top2').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500)
+    })
+
+})//document.ready
--- forest/js/main.js
+++ forest/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
 		navigation: true, /* 오른쪽에 각 페이지의 paging */
@@ -18,42 +19,41 @@ $(document).ready(function(){
 
 		afterLoad: function(origin, destination, direction, trigger){
 			if(destination.index == 0){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-				// console.log('1번째 슬라이드가 로딩 되었을때');
+				//console.log('1번째 슬라이드가 로딩 되었을때');
 				$('body').removeClass('bg_white')
-			}
-			else if(destination.index == 1){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-				// console.log('2번째 슬라이드가 로딩 되었을때');
+			}else if(destination.index == 1){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('2번째 슬라이드가 로딩 되었을때');
 				$('body').addClass('bg_white')
-			}
-			else if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-				// console.log('3번째 슬라이드가 로딩 되었을때');
+				/******** tree 숫자가 넘어가는 애니메이션 **********  */
+				$('.counter').counterUp();
+			}else if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('3번째 슬라이드가 로딩 되었을때');
 				$('body').removeClass('bg_white')
-			}
-			else if(destination.index == 3){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-				// console.log('4번째 슬라이드가 로딩 되었을때');
+			}else if(destination.index == 3){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('4번째 슬라이드가 로딩 되었을때');
 				$('body').addClass('bg_white')
 			}
 		},
 
-		responsiveWidth: 769, /* fullpage를 적용시키지 않을 모바일 사이즈 */
+		responsiveWidth: 1025, /* fullpage를 적용시키지 않을 모바일 사이즈 (768부터 모바일) */
         responsiveHeight: 700 /* 브라우저 높이가 700이하로 줄면 fullpage 안함 */
-	});
+	});//fullpage
 
-	/******************************************************************************
+
+	/******************************************************
 	 * aside quick 열고 닫기
-	 * 닫혀있을 때(open 클래스가 있을 때) - close 클래스 교체, detail 보임
-	 * 열려있을 때(open 클래스가 없을 때) - open 클래스 교체, detail 안보임
-	*/
+	 * 닫혀있을때 (open클래스가 있을때) - close클래스 교체, detail 보임
+	 * 열려있을때 (open클래스가 없을때) - open클래스 교체, detail 숨김
+	 */
+
 	$('.quick .btn').on('click', function(){
-		if($(this).hasClass('open') == true)
-		{
+		//console.log('누름!!!!!!!!!!!!!!!!!!!!!!!!')
+		if($(this).hasClass('open') == true){
 			$(this).removeClass('open')
 			$(this).addClass('close')
 			$(this).find('span').text('닫기')
 			$('.quick .detail').slideDown(200)
-		}
-		else
-		{
+		}else{
 			$(this).removeClass('close')
 			$(this).addClass('open')
 			$(this).find('span').text('열기')
@@ -61,11 +61,9 @@ $(document).ready(function(){
 		}
 	})
 
-	/********************************* visual swiper 추가 ******************************** */
-
+	/***************** visual swiper 추가 ********************* */
 	let visual_name = ['산림복원 기금 모집','같이가치 매달기부','서울마이트리','고목나무이야기']
-	
-
+	//console.log(visual_name[3]) 숫자가 0부터 시작
 	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
 		autoplay: {  /* 팝업 자동 실행 */
@@ -80,15 +78,31 @@ $(document).ready(function(){
 		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
 			el: '.visual .paging', /* 해당 요소의 class명 */
 			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-			// type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+			//type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
 			renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-				return '<span class="' + className + '">' + visual_name[index] + "</span>";
+				return '<span class="' + className + '">  ' + visual_name[index] + "</span>";
 			},
 		},
 
 	});
 
-	/**************** tree 숫자가 넘어가는 애니메이션 */
-	$('.counter').counterUp();
-
+	/******************************** story의 swiper **************************** */
+	const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			769: {    /* 1281~768px 적용 */
+				slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+			1281: {    /* 1280px 이상일때 적용 */
+				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	});
+	
+	
 })
