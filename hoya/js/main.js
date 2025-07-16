10e23d07a0326a70b42aab42399fd20324831830
tmdgus0232 <tmdgus0232@gmail.com>
2025-07-17 00:28:28 +0900

    250717

--- hoya/js/main.js
+++ hoya/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    // visual swiper
+
+    // sec01 swiper
     let visual_name = ['','',''];
     const sec01_swiper = new Swiper('.sec01 .swiper', {
         autoplay: {
@@ -14,26 +15,23 @@ $(document).ready(function(){
             prevEl: '.sec01 .ctrl_wrap button.btn_prev',  
         },
     });
-    // tech
-    $('.tech .list ul li').on('mouseenter', function(){
-        $('.tech .list ul li').removeClass('over');
+
+    // sec02 list
+    $('.sec02 .list ul li').on('mouseenter', function(){
+        $('.sec02 .list ul li').removeClass('over');
         $(this).addClass('over');
     });
 
-    const news_swiper = new Swiper('.product .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+    // sec03 swiper
+    const news_swiper = new Swiper('.product .swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 16,
         breakpoints: {
-            768: {    /* 640px 이상일때 적용 */
+            768: {
                 spaceBetween: 50,
             },
         },
-        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        // autoplay: {  /* 팝업 자동 실행 */
-        //     delay: 2500,
-        //     disableOnInteraction: true,
-        // },
+        loop: true,
         navigation: {
             nextEl: '.product .ctrl_wrap .btn_next',
             prevEl: '.product .ctrl_wrap .btn_prev',
