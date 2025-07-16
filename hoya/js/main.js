03f0af7ad48e91288fc742ac27223360a624d44a
tmdgus0232 <tmdgus0232@gmail.com>
2025-07-16 18:00:46 +0900

    250716

--- hoya/js/main.js
+++ hoya/js/main.js
@@ -1,33 +1,42 @@
 $(document).ready(function(){
-
-    /****************************** visual swiper ******************************/
-    let visual_name = ['','','']
-    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        autoplay: {  /* 팝업 자동 실행 */
+    // visual swiper
+    let visual_name = ['','',''];
+    const sec01_swiper = new Swiper('.sec01 .swiper', {
+        autoplay: {
             delay: 5000,
             disableOnInteraction: true,
         },
-        effect: "fade", /* fade 효과 */
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        effect: "fade",
+        loop: true,
 
-        navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.visual .ctrl_wrap button.btn_next',  /* 다음 버튼의 클래스명 */
-            prevEl: '.visual .ctrl_wrap button.btn_prev',  
+        navigation: {
+            nextEl: '.sec01 .ctrl_wrap button.btn_next',
+            prevEl: '.sec01 .ctrl_wrap button.btn_prev',  
         },
+    });
+    // tech
+    $('.tech .list ul li').on('mouseenter', function(){
+        $('.tech .list ul li').removeClass('over');
+        $(this).addClass('over');
+    });
 
-        // pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		// 	el: '.visual .paging', /* 해당 요소의 class명 */
-		// 	clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		// 	//type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-		// 	renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-		// 		return '<span class="' + className + '">  ' + visual_name[index] + "</span>";
-		// 	},
-		// },
+    const news_swiper = new Swiper('.product .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 640px 이상일때 적용 */
+                spaceBetween: 50,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+        navigation: {
+            nextEl: '.product .ctrl_wrap .btn_next',
+            prevEl: '.product .ctrl_wrap .btn_prev',
+        },
     });
-    /****************************** section - doctor aos ******************************/
-    AOS.init({
-        offset: 150, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
-        duration: 500, // 애니메이션 효과가 작동되는 시간
-        easing: 'ease', // 가속도
-    });
-})
+});
